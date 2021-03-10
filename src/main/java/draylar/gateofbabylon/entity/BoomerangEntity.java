39c292fe46abb77f7df3b67473fc1214d566519f
Draylar <samuelhimes@gmail.com>
2021-03-09 18:58:04 -0600

    add boomerang textures and entity/item pair

---
+++ src/main/java/draylar/gateofbabylon/entity/BoomerangEntity.java
@@ -0,0 +1,34 @@
+package draylar.gateofbabylon.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.Packet;
+import net.minecraft.world.World;
+
+public class BoomerangEntity extends Entity {
+
+    public BoomerangEntity(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Override
+    protected void initDataTracker() {
+
+    }
+
+    @Override
+    protected void readCustomDataFromTag(CompoundTag tag) {
+
+    }
+
+    @Override
+    protected void writeCustomDataToTag(CompoundTag tag) {
+
+    }
+
+    @Override
+    public Packet<?> createSpawnPacket() {
+        return null;
+    }
+}
---
+++ src/main/java/draylar/gateofbabylon/item/BoomerangItem.java
@@ -0,0 +1,16 @@
+package draylar.gateofbabylon.item;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class BoomerangItem extends Item {
+
+    public BoomerangItem(Settings settings) {
+        super(settings);
+    }
+}
