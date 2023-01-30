930259433db5d84071ca6a1e4ea16b9b6481b608
Draylar <17728338+Draylar@users.noreply.github.com>
2023-01-30 17:31:37 -0600

    Fix enchantment table bug from list change

--- src/main/java/draylar/gateofbabylon/mixin/EnchantmentHelperMixin.java
+++ src/main/java/draylar/gateofbabylon/mixin/EnchantmentHelperMixin.java
@@ -29,7 +29,7 @@ public class EnchantmentHelperMixin {
     private static void generateEnchantments(Random random, ItemStack stack, int level, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir, List<EnchantmentLevelEntry> list, Item item, int i, float f, List<EnchantmentLevelEntry> list2) {
         List<EnchantmentLevelEntry> newEnchantments = new ArrayList<>();
 
-        list.forEach(enchantmentLevelEntry -> {
+        list2.forEach(enchantmentLevelEntry -> {
             if (enchantmentLevelEntry.enchantment instanceof ValidatingEnchantment) {
                 if (enchantmentLevelEntry.enchantment.isAcceptableItem(stack)) {
                     newEnchantments.add(enchantmentLevelEntry);
